80ba242e84a904b302a359bba668e4d3e5556a78
YifanZHANG-newsbreak <yifan.zhang@newsbreak.com>
2025-07-22 14:03:51 +0800

    not validate full schema, only check new fields instead

--- core/src/main/java/io/confluent/kafka/schemaregistry/storage/KafkaSchemaRegistry.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/storage/KafkaSchemaRegistry.java
@@ -503,12 +503,12 @@ public class KafkaSchemaRegistry implements SchemaRegistry, LeaderAwareSchemaReg
 //        }
 //        isAddOnlyCompatible = sequentialSchemaInOrderCheckErrMsgs.isEmpty();
 //      }
-      if (schema.getSchemaType().equals("AVRO") && undeletedVersions.isEmpty()) {
-        List<String> checkResult = ((AvroSchema) parsedSchema).check();
-        if (!checkResult.isEmpty()) {
-          throw new IncompatibleSchemaException(checkResult.get(0));
-        }
-      }
+//      if (schema.getSchemaType().equals("AVRO") && undeletedVersions.isEmpty()) {
+//        List<String> checkResult = ((AvroSchema) parsedSchema).check();
+//        if (!checkResult.isEmpty()) {
+//          throw new IncompatibleSchemaException(checkResult.get(0));
+//        }
+//      }
 
       if (normalize) {
         parsedSchema = parsedSchema.normalize();
